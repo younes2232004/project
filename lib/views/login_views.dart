5fa6d88f6fcb03518617908c56d0ec707d88c299
younis <yalmayth88@gmail.com>
2025-02-04 14:51:02 +0300

    views 4/2/2025 2:50

---
+++ lib/views/login_views.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+// safe login information in the app (internally)
+// force user to signup before using the app
+// but other app , let user use app without signup but after some time or some action , force user to signup
+class LoginView extends StatelessWidget {
+  const LoginView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // logo of the app
+    // input field for email
+    // input field for password
+    // button for login
+    // button for signup
+    return Container();
+  }
+}
---
+++ lib/views/signup_views.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SignupView extends StatelessWidget {
+  const SignupView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // logo of the app
+    // input field for email
+    // input field for password
+    // input field for confirm password
+    // button for signup
+    // after that direct to login view or home view based on user data
+    return Container();
+  }
+}
