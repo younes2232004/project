b0aa9963b4ba6b986dc786234573622335660979
younis <yalmayth88@gmail.com>
2025-04-21 09:34:50 +0300

    project

---
+++ project-main/lib/views/home_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // depends on project requirement we can show the home view
+    // we will use layoutout widgets like column, row, stack, gridview, listview, etc
+    // we will use the app bar, bottom navigation bar, drawer, etc
+    // we will use the card, listtile, etc
+    return Container();
+  }
+}
---
+++ project-main/lib/views/onboarding_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OnboardingView extends StatelessWidget {
+  const OnboardingView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // we will use pageview widget
+    // onboarding view with logo of the app and title and description of the app
+    // onboarding vwith logo of the app and title and description of the app
+    // onboarding with logo of the app and title and description of the app
+    // direct to login view  or home view based on user data
+    return Container();
+  }
+}
